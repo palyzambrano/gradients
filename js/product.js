6c90d55ffb32d47c8c3924b59b30102942b939bf
Paulina Zambrano <palyzambrano@gmail.com>
2022-06-22 16:11:56 -0400

    agrega pagina producto

--- js/product.js
+++ js/product.js
@@ -3,6 +3,24 @@ var link = new URL(window.location.href);
 // Usando el objeto URL tomo el query param para producto
 var producto = link.searchParams.get('producto');
 
-// Escribe el contenido del JSON en el query param
-// en el documento
-document.writeln(producto);
+var productoObject = JSON.parse(producto);
+
+
+var photoEl = $('#photo');
+photoEl.attr('src', productoObject.photo);
+
+var brandEl = $('#brand');
+brandEl.text(productoObject.brand);
+
+var titleEl = $('#title');
+titleEl.text(productoObject.name);
+
+var descriptionEl = $('#description');
+descriptionEl.text(productoObject.description);
+
+var attributesEl = $('#attributes');
+attributesEl.text(productoObject.attributes)
+
+
+var useEl = $('#howUse')
+useEl.text(productoObject.how_to_use)
